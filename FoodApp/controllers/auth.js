e11cd396ea30fb1946e1359410fc34bd6d1924af
Tzuzul <mail@tzuzulcode.com>
2022-07-08 08:14:53 -0500

    Orders

--- FoodApp/controllers/auth.js
+++ FoodApp/controllers/auth.js
@@ -13,24 +13,37 @@ class AuthController{
         const {name,email,password,birthday} = req.body
         // SELECT id,name,email FROM users
         try {
-            const user = await client.user.create({
-                select:{
-                    id:true,
-                    name:true,
-                    email:true,
-                    role:true
-                },
+            let user = await client.user.create({
                 data:{
                     name,
                     email,
                     birthday: new Date(birthday),
-                    password: await encrypt(password)
+                    password: await encrypt(password),
+                    orders:{
+                        create:{
+                            completed:false
+                        }
+                    },
+                },
+                include:{
+                    orders:true
                 }
             })
 
+            const userWithOrder = await client.user.update({
+                where:{
+                    id:user.id
+                },
+                data:{
+                    activeOrder: user.orders[0].id
+                }
+            })
+
+            delete userWithOrder.password
+
             req.session.user = {
                 loggedIn:true,
-                ...user //spread operator
+                ...userWithOrder //spread operator
             }
             return res.redirect("/")
         } catch (error) {
@@ -50,6 +63,8 @@ class AuthController{
                 email:email
             }
         })
+
+        console.log(user)
 
         if(user && await compare(password,user.password)){
             delete user.password
--- FoodApp/controllers/orders.js
+++ FoodApp/controllers/orders.js
@@ -1,22 +1,127 @@
+const prisma = require("../libs/db")
+
 class OrdersController{
     static async getActualOrder(req,res){
+        const {activeOrder} = req.session.user
+        
+        const order = await prisma.order.findUnique({
+            where:{
+                id: activeOrder
+            },
+            include:{
+                food:{
+                    include:{
+                        food:true
+                    }
+                }
+            }
+        })
 
+        return res.render("order",{
+            order
+        })
     }
 
     static async getCompletedOrders(req,res){
+        const {id} = req.session.user
+        const orders = await prisma.order.findMany({
+            where:{
+                userID:id,
+                completed:true
+            },
+            include:{
+                food:{
+                    include:{
+                        food:true
+                    }
+                }
+            }
+        })
 
+
+        return res.render("orders",{
+            orders
+        })
     }
 
     static async makeOrderCompleted(req,res){
-        
+        const {activeOrder,id} = req.session.user
+        const order = await prisma.order.update({
+            where:{
+                id:activeOrder
+            },
+            data:{
+                completed:true
+            }
+        })
+
+        const newOrder = await prisma.order.create({
+            data:{
+                completed:false,
+                user:{
+                    connect:{
+                        id
+                    }
+                }
+            }
+        })
+
+        const user = await prisma.user.update({
+            data:{
+                activeOrder:newOrder.id
+            },
+            where:{
+                id
+            }
+        })
+
+        req.session.user.activeOrder = newOrder.id
+
+        return res.redirect("/orders/completed")
     }
 
     static async addFood(req,res){
+        const {activeOrder} = req.session.user
+        const {idFood} = req.params
 
+        const order = await prisma.order.update({
+            where:{
+                id:activeOrder
+            },
+            data:{
+                food:{
+                    create:{
+                        food:{
+                            connect:{
+                                id:parseInt(idFood)
+                            }
+                        }
+                    }
+                }
+            }
+        })
+
+        return res.redirect("/orders")
     }
 
     static async deleteFood(req,res){
+        const {activeOrder} = req.session.user
+        const {idFood} = req.params
 
+        const order = await prisma.order.update({
+            where:{
+                id:activeOrder
+            },
+            data:{
+                food:{
+                    deleteMany:{
+                        foodID:parseInt(idFood)
+                    }
+                }
+            }
+        })
+
+        return res.redirect("/orders")
     }
 
 }
